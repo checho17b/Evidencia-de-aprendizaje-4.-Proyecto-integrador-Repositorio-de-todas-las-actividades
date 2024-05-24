665f2291fdb66d0b83aa980480d2bbf8818648a2
checho17b <115435490+checho17b@users.noreply.github.com>
2024-05-24 18:18:02 -0500

    Add files via upload

---
+++ Evidencia de aprendizaje 3/QUERY CREA TABLAS STAGING - ENTREGA 3.sql
@@ -0,0 +1,105 @@
+USE jardineria
+
+/*Manejo de ROLLBACK ante fallos*/
+BEGIN TRY
+	BEGIN TRAN
+	
+		/*CODIGO VALIDA Y BORRA TABLA TEMPORAL VENTAS*/
+	DROP TABLE IF EXISTS VENTAS
+
+	/*CONSULTA CREA TABLA VENTAS TEMPORAL*/
+
+	IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'VENTAS') AND type in (N'U'))
+	BEGIN
+		SELECT DISTINCT p.ID_pedido AS VentasID, p.fecha_pedido AS FECHA, c.ID_cliente AS ClienteID, c.nombre_cliente, 
+		e.ID_oficina AS OficinaID, O.Descripcion AS Oficina,dp.ID_producto AS ProductoID, pro.nombre
+		INTO VENTAS
+		FROM pedido p
+		INNER JOIN cliente c ON p.ID_cliente = c.ID_cliente
+		INNER JOIN oficina O ON O.ID_oficina = C.ID_cliente
+		INNER JOIN empleado e ON c.ID_empleado_rep_ventas = e.ID_empleado
+		INNER JOIN detalle_pedido dp ON p.ID_pedido = dp.ID_pedido
+		INNER JOIN producto PRO ON CAST(DP.ID_producto AS varchar) = dp.ID_producto
+		ORDER BY FECHA
+	END
+
+	
+		/*CONSULTA CREA TABLA TIEMPO*/
+
+	DROP TABLE IF EXISTS TIEMPO
+
+	IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'TIEMPO') AND type in (N'U'))
+	BEGIN
+		SELECT  DISTINCT P.ID_cliente,C.nombre_cliente, YEAR(fecha_pedido) AS AÑO
+		, MONTH(fecha_pedido) AS MES,DAY(fecha_pedido) AS DIA
+		INTO TIEMPO
+		FROM pedido P
+		JOIN VENTAS V ON V.ClienteID = P.ID_cliente
+		JOIN cliente C ON C.ID_cliente = V.ClienteID
+		ORDER BY AÑO, DIA DESC
+
+	END	
+	
+		SELECT * FROM TIEMPO
+	/* CREA TABLA VENTAS NEW*/
+
+	DROP TABLE IF EXISTS VENTAS
+
+		SELECT DISTINCT p.ID_pedido AS VentasID, p.fecha_pedido AS FECHA, c.ID_cliente AS ClienteID, c.nombre_cliente, 
+		e.ID_oficina AS OficinaID, O.Descripcion AS Oficina,T.ID_cliente AS ID_TIEMPO, dp.ID_producto AS ProductoID, pro.nombre
+		INTO VENTAS
+		FROM pedido p
+		INNER JOIN cliente c ON p.ID_cliente = c.ID_cliente
+		INNER JOIN oficina O ON O.ID_oficina = C.ID_cliente
+		INNER JOIN empleado e ON c.ID_empleado_rep_ventas = e.ID_empleado
+		INNER JOIN detalle_pedido dp ON p.ID_pedido = dp.ID_pedido
+		INNER JOIN producto PRO ON CAST(DP.ID_producto AS varchar) = dp.ID_producto
+		INNER JOIN TIEMPO t on t.[ID_cliente] = c.ID_cliente
+		ORDER BY FECHA
+
+		SELECT * FROM VENTAS
+	/*CONSULTA CREA TABLA CLIENTE*/
+	
+	SELECT DISTINCT C.nombre_cliente,c.telefono, C.pais,C.region, C.ciudad, c.codigo_postal
+	FROM cliente C
+	ORDER BY nombre_cliente, pais
+
+	/*CONSULTA CREA TABLA PRODUCTO*/
+	SELECT 
+		p.ID_producto AS 'orden id',
+		p.nombre AS 'nombre_producto',
+		c.Desc_Categoria AS 'categoria',
+		p.descripcion AS 'descripcion'
+	FROM 
+		producto p
+	JOIN 
+		Categoria_producto c ON p.Categoria = c.Desc_Categoria
+
+	/*CONSULTA CREA TABLA OFICINA*/
+	--Esta esta super bien.
+	SELECT ID_oficina,
+		Descripcion,
+		ciudad,
+		pais,
+		region,
+		codigo_postal,
+		telefono,
+		linea_direccion1
+	FROM oficina
+
+	
+
+   COMMIT TRAN
+END TRY
+BEGIN CATCH
+    -- Captura y muestra el error
+    SELECT ERROR_MESSAGE() AS ErrorMessage, 
+           ERROR_NUMBER() AS ErrorNumber, 
+           ERROR_SEVERITY() AS ErrorSeverity, 
+           ERROR_STATE() AS ErrorState, 
+           ERROR_LINE() AS ErrorLine, 
+           ERROR_PROCEDURE() AS ErrorProcedure;
+END CATCH;
+
+  
+ 
